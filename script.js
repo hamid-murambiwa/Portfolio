79969c921ffe14a9031e5afa31e024a9ce59baf5
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-08-30 20:01:03 +0200

    Add event listeners for menu button and menu items. Update styling for active menu.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const menuBtn = document.querySelector('.menu');
 const menuList = document.querySelector('.navlinks');
 const menuItems = Array.from(document.querySelectorAll('.navlink-items'));
+const menuIcon = document.querySelector('.menu-icons');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    menuList.classList.toggle('active');
+
+    if (menuBtn.classList.contains('active')) {
+        menuIcon.src = './images/Disabled.svg';
+    } else {
+        location.reload();
+    }
+});
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuBtn.classList.toggle('active');
+        menuList.classList.toggle('active');
+        menuIcon.src = './images/Menu.svg';
+    })
+})
